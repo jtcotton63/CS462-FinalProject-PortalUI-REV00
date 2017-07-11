e96eaed1e11cba8856ac0c9f567bbf506751e5c3
Joseph Cotton <jtcotton63@gmail.com>
2017-07-11 09:23:51 -0600

    Index page correctly loads user details

---
+++ server.js
@@ -0,0 +1,82 @@
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var express = require('express');
+var fs = require('fs');
+
+
+
+// 
+var app = express();
+var userService = require('./service/UserService');
+
+
+
+// Config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+
+
+
+// Configure router
+var router = express.Router();
+
+// Standard routes
+router.get('/', function(req, res) {
+    // Determine if they have a cookie
+    var userIdCookie = req.cookies.userId;
+    if(userIdCookie === undefined) {
+        res.redirect('/login');
+    }
+    
+    else {
+        res.sendFile(__dirname + '/views/index.html');
+    }
+});
+
+router.post('/authenticate', function(req, res) {
+    userService.authenticate(req, res, function(req, res, data) {
+        console.log('Authenticated returned data: ' + JSON.stringify(data));
+        
+        if(data.authenticated === true) {
+            var id = data.userId;
+            res.cookie("userId", id);
+            res.redirect('/');
+        }
+        
+        else {
+            res.status(401);
+            res.send({error: "Invalid username and/or password"});
+        }
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+router.get('/logout', function(req, res) {
+    // Remove the cookie 
+    res.clearCookie('id');
+    res.redirect('/login');
+});
+
+// User service calls
+router.get('/services/user/users/:userId', function(req, res) {
+    var userId = req.params.userId;
+    userService.getUserById(req, res, userId, function(req, res, data) {
+       res.json(data);
+    });
+});
+
+app.use('/', router);
+
+
+
+// Start the app
+var port = 8080;
+app.listen(port, function () {
+    console.log('Server started on port ' + port);
+});
---
+++ service/UserService.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var serviceBase = 'ec2-34-211-205-116.us-west-2.compute.amazonaws.com';
+var servicePort = 8080;
+
+exports.authenticate = function(req, res, callback) {
+    var options = {
+        host: serviceBase,
+        path: '/authenticate',
+        port: servicePort,
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+    };
+    
+    var request = http.request(options, function(response) {
+        var dataString = '';
+        response.on('data', function(chunk) {
+            dataString += chunk;
+        });
+        response.on('end', function() {
+           var data = JSON.parse(dataString);
+           console.log('On data end: ' + dataString);
+           callback(req, res, data);
+        });
+    });
+    
+    var requestBody = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    
+    request.write(JSON.stringify(requestBody));
+    request.end();
+};
+
+exports.getUserById = function(req, res, userId, callback) {
+    var options = {
+        host: serviceBase,
+        path: '/users/' + userId,
+        port: servicePort,
+        headers: {'Content-Type': 'application/json'}
+    };
+    
+    http.get(options, function(response) {
+        var dataString = '';
+        response.on('data', function(chunk) {
+            dataString += chunk;
+        });
+        response.on('end', function() {
+           var data = JSON.parse(dataString);
+           console.log('On data end: ' + dataString);
+           callback(req, res, data);
+        });
+    });
+};
